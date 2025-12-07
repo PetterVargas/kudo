e85d067d109b8ac8d6dd79168b069c47c1192e48
Peter Vargas <peter.vargasg@gmail.com>
2025-12-07 11:08:25 -0500

    fix deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="es" suppressHydrationWarning>
       <body>
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-XEZ2YDSE2K"
+          src="https://www.googletagmanager.com/gtag/js?id=G-QCM48B2CJQ"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -55,7 +55,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-XEZ2YDSE2K');
+            gtag('config', 'G-QCM48B2CJQ');
           `}
         </Script>
         <RootProvider
